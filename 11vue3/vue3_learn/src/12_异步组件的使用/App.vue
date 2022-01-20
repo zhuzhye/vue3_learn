182fa355163b1bd1b7593c9eebccbc4eca69e56e
zhuzheye <823320625@qq,com>
2022-01-21 02:59:57 +0800

    动态组件

---
+++ 11vue3/vue3_learn/src/12_异步组件的使用/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+  <AsyncCategory></AsyncCategory>
+</template>
+
+<script>
+import Home from "./Home.vue";
+import { defineAsyncComponent } from "vue";
+import loadingVue from "./loading.vue";
+// const AsyncCategory =defineAsyncComponent(()=>import("./AsyncCategory.vue"))
+const AsyncCategory =defineAsyncComponent({
+  loader:()=>import("./AsyncCategory.vue"),
+  loadingComponent:loadingVue,
+  // errorComponent
+  //在显示loadingcompoent之前等待多长事件
+  delay:2000
+})
+export default {
+  components: {
+    Home,
+    AsyncCategory
+  },
+};
+</script>
+
+<style scoped></style>
